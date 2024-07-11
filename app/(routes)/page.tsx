8018645be50d0a7868b41f81be848ebdd933d0ac
Vince Abella <vinceabella05@gmail.com>
2024-07-12 03:00:28 +0800

    changes in the layout

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,25 +1,30 @@
-import Image from 'next/image';
-import Button from '../components/Button';
-import ButtonsGroup from '../layouts/ButtonsGroup';
-import Navbar from '../components/UI/Navbar';
 import HeaderOne from '../components/Landing/HeaderOne';
 import LeftOne from '../components/Landing/LeftOne';
 import RightOne from '../components/Landing/RightOne';
 
 export default function Home() {
+	const dataToPass = {
+		header: 'Props Passing',
+		subHeader: 'Learning how to pass props from parent component to the children.',
+	};
+
+	const content = 'This is a sample content to pass on the leftOne card component.';
+
 	return (
 		<main className='flex min-h-screen flex-col'>
-			<div className='max-w-7xl w-full h-fit mx-auto p-4 gap-4 flex flex-col'>
+			<div className='max-w-7xl w-full h-fit mx-auto md:p-10 p-4 gap-4 flex flex-col'>
 				<HeaderOne />
-				<div className='flex max-w-7xl w-full items-center gap-2 justify-between mx-auto'>
+				<div className='flex max-w-7xl w-full items-center gap-4 justify-between mx-auto'>
 					<div className='w-1/2'>
-						<LeftOne />
+						<LeftOne
+							content={content}
+							data={dataToPass}
+						/>
 					</div>
 					<div className='w-1/2'>
 						<RightOne />
 					</div>
 				</div>
-				<RightOne />
 			</div>
 		</main>
 	);
--- app/components/Landing/HeaderOne.tsx
+++ app/components/Landing/HeaderOne.tsx
@@ -1,28 +1,30 @@
 import Image from 'next/image';
 import React from 'react';
-import Diamond from '@/public/blue-diamond.png';
 
 const HeaderOne = () => {
 	return (
 		<div className='bg-gradient-to-tr from-blue-900 to-blue-500 w-full h-64 rounded-xl text-custom-white-100 flex p-10 items-end justify-center relative'>
-			<div className='absolute h-fit z-0'>
+			<div className='absolute h-full z-0 top-0 p-10'>
 				<Image
-					src={Diamond}
+					src={'https://i.pinimg.com/originals/e3/82/52/e3825274a94bafc3f0282cae29c19972.png'}
 					alt={'blue diamond'}
 					width={200}
 					height={200}
-					style={{ objectFit: 'contain' }}
+					className=' invert'
+					style={{ objectFit: 'contain', width: 'auto', height: '100%' }}
 				/>
 			</div>
-			<div className='w-full flex items-start h-fit z-10'>
-				<h1 className='font-normal text-6xl w-1/2'>Spaceport</h1>
-				<h2 className='w-1/3 text-center'>
-					Accelerating the growth of <br />
-					the next digital economy
-				</h2>
-				<button className='text-sm font-semibold px-6 py-3 border border-white rounded-full h-fit w-fit text-nowrap uppercase'>
-					Get Early Access
-				</button>
+			<div className='w-full flex items-start h-fit z-10 gap-20'>
+				<h1 className='font-bold text-6xl w-1/2 uppercase'>What&apos;s Next?</h1>
+				<div className='w-1/2 flex justify-between items-start'>
+					<h2 className='w-full text-center'>
+						Accelerating the growth of <br />
+						the next digital economy
+					</h2>
+					<button className='text-sm font-semibold px-6 py-3 border border-white hover:bg-white hover:text-blue-500 rounded-full h-fit w-fit text-nowrap uppercase'>
+						Get Early Access
+					</button>
+				</div>
 			</div>
 		</div>
 	);
